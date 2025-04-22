7aa43c5dc85a3c1e7119cfc1f19916c64d864d0d
Osiris HP <Usir95.hp@gmail.com>
2025-04-22 14:34:04 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'LaboratiorioMagico',
+  webDir: 'dist'
+};
+
+export default config;
