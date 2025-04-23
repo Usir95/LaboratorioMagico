be8d2475d88ee53b35f7d258086bc85a7454586a
Osiris HP <Usir95.hp@gmail.com>
2025-04-23 13:37:13 -0600

    Instalacion y configuracion de tailwind css

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import App from './App.vue'
 import router from './router';
 
 import { IonicVue } from '@ionic/vue';
-import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader' // <-- esto es lo nuevo
+import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader'
+
+import './theme/tailwind.css';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
